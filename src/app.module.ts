e1c447a48b9a07c0e64fcfad8cb5fb8bef3784d6
000-dir-wmi-0001 <momindy321@gmail.com>
2025-11-29 14:50:14 +0530

    fixes issue

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChatGateway } from './chat.gateway';
+import { VideoCallGateway } from './video-call.gateway';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, ChatGateway],
+  providers: [AppService, ChatGateway, VideoCallGateway],
 })
 export class AppModule {}
